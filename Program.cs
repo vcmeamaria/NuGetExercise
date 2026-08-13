d48765456e894460803c8e8a94a21eff0d584cb1
Maria <115981159+vcmeamaria@users.noreply.github.com>
2026-08-13 14:13:27 +0100

    Add Newtonsoft JSON package and serialization example

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+
+var student = new
+{
+    Name = "Maria",
+    Course = "Cyber Security"
+};
+
+string json = JsonConvert.SerializeObject(student, Formatting.Indented);
+
+Console.WriteLine("Student as JSON:");
+Console.WriteLine();
+Console.WriteLine(json);
